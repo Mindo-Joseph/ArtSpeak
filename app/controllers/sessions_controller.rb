9d3193815c7a4988a031a23d87221a9bdf4257e4
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-08-12 18:37:07 +0300

    Create user show page and follow-partials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user
       log_in user
       redirect_to root_url
